7373b57eab890bcd24e59393eaf71b8df1c98f12
1105075896@qq.com <1105075896@qq.com>
2019-01-05 14:47:40 +0800

    代理请求新增AES加密 优化代理请求时数据传递

--- app/src/main/java/com/monke/monkeybook/base/ProxyInterceptor.java
+++ app/src/main/java/com/monke/monkeybook/base/ProxyInterceptor.java
@@ -22,15 +22,19 @@ public class ProxyInterceptor implements Interceptor {
         if (ProxyManager.hasProxy()) {  //如果是代理模式则优先请求代理服务器，失败再自行本地请求
             String url = request.url().toString();
             if (!StringUtil.isBlank(url)) {
-                url = URLEncoder.encode("url", "utf-8");
+                url = URLEncoder.encode(url, "utf-8");
             }
-            Request requestProxy = new Request.Builder()
-                    .url(ProxyManager.proxyHttp)
-                    .header("url", url)
-                    .header("packagename",ProxyManager.packAgeEncode)
-                    .get()
-                    .build();
-            Response responseProxy = chain.proceed(request);
+            Request.Builder requestProxyBuilder = new Request.Builder()
+                    .url(ProxyManager.proxyHttp);
+            requestProxyBuilder.headers(request.headers());
+            requestProxyBuilder.header("proxyUrl", url)
+                    .header("proxyPackagename",ProxyManager.packAgeEncode);
+            if(request.method().equalsIgnoreCase("get")){
+                requestProxyBuilder.get();
+            }else{
+                requestProxyBuilder.post(request.body());
+            }
+            Response responseProxy = chain.proceed(requestProxyBuilder.build());
             if(responseProxy.isSuccessful()){
                 return responseProxy;
             }
